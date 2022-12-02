46a6946b0942df697cc4dec9e3c9a48ec203e217
Lev Vereshchagin <mail@vrslev.com>
2022-12-02 13:34:11 +0300

    Add coordinates offset

--- src/geodecoder/api.py
+++ src/geodecoder/api.py
@@ -1,4 +1,6 @@
 import sys
+from collections import defaultdict
+from decimal import Decimal
 from typing import Any, Protocol
 
 
@@ -12,12 +14,29 @@ def _build_url(api_key: str, address: str) -> str:
     return f"https://geocode-maps.yandex.ru/1.x/?geocode={address}&lang=ru_RU&result=1&apikey={api_key}&format=json"
 
 
+_lat_offsets: dict[Decimal, int] = defaultdict(int)
+_lon_offsets: dict[Decimal, int] = defaultdict(int)
+
+
+def _offset_coordinates(lat: Decimal, lon: Decimal) -> tuple[Decimal, Decimal]:
+    offset = Decimal(0.0001)
+
+    if _lat_offsets[lat] <= _lon_offsets[lon]:
+        _lat_offsets[lat] += 1
+    else:
+        _lon_offsets[lon] += 1
+
+    return lat + _lat_offsets[lat] * offset, lon + _lon_offsets[lon] * offset
+
+
 def _parse_response(response: dict[str, Any]) -> tuple[str, str]:
     if "response" not in response:
         raise Exception(response)
-    return response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"][
-        "Point"
-    ]["pos"].split(" ")
+    lat, lon = response["response"]["GeoObjectCollection"]["featureMember"][0][
+        "GeoObject"
+    ]["Point"]["pos"].split(" ")
+    result = _offset_coordinates(Decimal(lat), Decimal(lon))
+    return str(result[0]), str(result[1])
 
 
 if sys.platform == "emscripten":
